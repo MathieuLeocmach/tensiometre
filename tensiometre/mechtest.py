1af433976bcea1c12dbd1bfa716dba6433bf2c69
Manip (local) <manip@localhost.localdomain>
2018-11-05 16:27:13 +0100

    Add move_to_constant_position and stay_constant_position.

--- tensiometre/mechtest.py
+++ tensiometre/mechtest.py
@@ -3,6 +3,44 @@ from contextlib import closing
 from tensiometre.dt3100 import DT3100, ReadOne, recover, read_both, ReadDuration
 from tensiometre.mpc385 import MPC385
 
+def move_to_constant_position(outname, ab2xz, kp=0.9, dz =-100, dx=0, moveback=False):
+    """Moving the absolute position of the head by dz and dx and stay at that position using PID feedback. Need ab2xz calibration matrix."""
+    with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
+        sensors = [sensorA, sensorB]
+        #setting up sensors
+        for sensor in sensors:
+            sensor.set_averaging_type(3)
+            sensor.set_averaging_number(3)
+        #remember original positions of the sensors and actuator
+        X0, Z0 = np.matmul(ab2xz, read_both(sensorA, sensorB))
+        x0,y0,z0 = actuator.update_current_position()[1:]
+        #setting up PID
+        pids = []
+        for s in [x0, z0] - actuator.um2step(np.array([X0+dx, Z0+dz])):
+            pid = PID(kp, 0, 0)
+            pid.setPoint = s
+            pids.append(pid)
+        try:
+            with open(outname, "wb") as fout:
+                m = moverPID.constant_position_XZ(sensors, actuator, ab2xz, pids, outputFile=fout)
+                m.start()
+                try:
+                    while True:
+                        time.sleep(1)
+                except KeyboardInterrupt:
+                    pass
+                #stop PID
+                m.go = False
+                m.join()
+        finally:
+            if moveback:
+                #move the actuator back to its original position
+                actuator.move_to(x0, y0, z0)
+                
+def stay_constant_position(outname, ab2xz, kp=0.9, moveback=False):
+    """Keep the absolute position of the head constant using PID feedback. Need ab2xz calibration matrix."""
+    move_to_constant_position(outname, ab2xz, kp, dz=0, dx=0, moveback=False)
+
 
 def traction(dz = -3000, velocity=2, duration = 1., avt=3, avn=3, outputname=None):
     """Perform a traction test, moving in z by dz (in microns, pointing down) at a velocity imposed by actuator. Aquires in avt avn mode for a fixed duration."""
