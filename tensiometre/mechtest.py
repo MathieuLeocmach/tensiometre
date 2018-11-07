804ce9520881cc639e0ee8128084d4c9f7932a34
Manip (local) <manip@localhost.localdomain>
2018-11-07 17:36:28 +0100

    add move_to_constant_positions to to multiple strain steps.

--- tensiometre/mechtest.py
+++ tensiometre/mechtest.py
@@ -6,7 +6,7 @@ from tensiometre.mpc385 import MPC385
 from tensiometre.pid import PID
 from tensiometre import moverPID
 
-def move_to_constant_position(outname, ab2xz, kp=0.9, dz =-100, dx=0, duration=None, moveback=False):
+def move_to_constant_positions(ab2xz, outnames, dxs, dzs, durations, kp=0.9, moveback=False):
     """Moving the absolute position of the head by dz and dx and stay at that position using PID feedback. Need ab2xz calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
     with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
         sensors = [sensorA, sensorB]
@@ -19,27 +19,35 @@ def move_to_constant_position(outname, ab2xz, kp=0.9, dz =-100, dx=0, duration=N
         x0,y0,z0 = actuator.update_current_position()[1:]
         #setting up PID
         pids = []
-        for s in [x0, z0] + actuator.um2step(np.array([X0+dx, Z0+dz])):
+        initialposition = [x0, z0] + actuator.um2step(np.array([X0, Z0]))
+        for s in initialposition:
             pid = PID(kp, 0, 0)
             pid.setPoint = s
             pids.append(pid)
         try:
-            with open(outname, "wb") as fout:
-                m = moverPID.constant_position_XZ(sensors, actuator, ab2xz, pids, outputFile=fout)
-                t0 = time.time()
-                m.start()
-                try:
-                    while (duration is None) or (time.time() < t0 + duration):
-                        time.sleep(1)
-                except KeyboardInterrupt:
-                    pass
-                #stop PID
-                m.go = False
-                m.join()
+            for outname, dx, dz, duration in zip(outnames, dxs, dzs, durations):
+                for s, pid in zip(initialposition + actuator.um2step(np.array([dx,dz])), pids):
+                    pid.setPoint = s
+                with open(outname, "wb") as fout:
+                    m = moverPID.constant_position_XZ(sensors, actuator, ab2xz, pids, outputFile=fout)
+                    t0 = time.time()
+                    m.start()
+                    try:
+                        while (duration is None) or (time.time() < t0 + duration):
+                            time.sleep(1)
+                    except KeyboardInterrupt:
+                        pass
+                    #stop PID
+                    m.go = False
+                    m.join()
         finally:
             if moveback:
                 #move the actuator back to its original position
                 actuator.move_to(x0, y0, z0)
+
+def move_to_constant_position(outname, ab2xz, kp=0.9, dz =-100, dx=0, duration=None, moveback=False):
+    """Moving the absolute position of the head by dz and dx and stay at that position using PID feedback. Need ab2xz calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
+    move_to_constant_positions(ab2xz, outnames=[outname], dxs=[dx], dzs=[dz], durations=[duration], kp=kp, moveback=moveback)
                 
 def stay_constant_position(outname, ab2xz, kp=0.9, duration=None, moveback=False):
     """Keep the absolute position of the head constant using PID feedback. Need ab2xz calibration matrix."""
