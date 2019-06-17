53a81d6e6c2820406e5169a96df2554cd4ceda3a
Manip (local) <manip@localhost.localdomain>
2019-06-17 14:38:58 +0200

    add_constant_deflection

--- tensiometre/mechtest.py
+++ tensiometre/mechtest.py
@@ -37,14 +37,67 @@ def move_to_constant_positions(ab2xy, outnames, dxs, dys, durations, kp=0.9, mov
                             time.sleep(1)
                     except KeyboardInterrupt:
                         pass
-                    #stop PID
-                    m.go = False
-                    m.join()
+                    finally:
+                        #stop PID
+                        m.go = False
+                        m.join()
+        finally:
+            if moveback:
+                #move the actuator back to its original position
+                actuator.move_to(x0, y0, z0)
+                
+def add_constant_deflection(ab2xy, outnames, dXs, dYs, durations, kp=0.9, moveback=False):
+    """Considering initial deflection is (0,0), successively add dX and dY to the deflection during duration and stay at this deflection using using PID feedback (constant stress). Iterate on next dX, dY, duration. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
+
+    #remember original positions of the sensors and actuator
+    with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
+        sensors = [sensorA, sensorB]
+        #setting up sensors
+        for sensor in sensors:
+            sensor.set_averaging_type(3)
+            sensor.set_averaging_number(3)
+        #remember initial state
+        x0,y0,z0 = actuator.update_current_position()[1:]
+        X0, Y0 = np.matmul(ab2xy, read_both(sensorA, sensorB))
+        #setting up PID
+        pids = []
+        initialposition = (np.array([X0, Y0]))
+        for s in initialposition:
+            pid = PID(kp, 0, 0)
+            pid.setPoint = s
+            pids.append(pid)
+        try:
+            for dX, dY, duration, outname in zip(dXs, dYs, durations, outnames):
+                for s, pid in zip(
+                    np.array([X0,Y0]) + np.array([dX, dY]), 
+                    pids
+                ):
+                    pid.setPoint = s
+                with open(outname, "wb") as fout:
+                    m = moverPID.constant_deflection_XY(
+                        sensors, actuator, 
+                        ab2xy, pids, outputFile=fout
+                        )
+                    t0 = time.time()
+                    m.start()
+                    try:
+                        while (duration is None) or (time.time() < t0 + duration):
+                            time.sleep(1)
+                    except KeyboardInterrupt:
+                        pass
+                    finally:
+                        #stop PID
+                        m.go = False
+                        m.join()
         finally:
             if moveback:
                 #move the actuator back to its original position
                 actuator.move_to(x0, y0, z0)
 
+def stay_constant_deflection(outname, ab2xy, kp=0.9, duration=None, moveback=False):
+    """keep the head at constant position from the sensor"""             
+    move_to_constant_deflection(ab2xy, outnames = [outname], dxs = [0], dys = [0],durations=[duration], kp=kp, moveback=moveback)
+    
 def move_to_constant_position(outname, ab2xy, kp=0.9, dy =-100, dx=0, duration=None, moveback=False):
     """Moving the absolute position of the head by dy (depth) and dx (width) and stay at that position using PID feedback. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
     move_to_constant_positions(ab2xy, outnames=[outname], dxs=[dx], dys=[dy], durations=[duration], kp=kp, moveback=moveback)
