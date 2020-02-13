4c6a99c78484a6db3795beb71751df9b991b4d19
mleocmach <mathieu.leocmach@univ-lyon1.fr>
2020-02-13 11:45:42 +0100

    allow for different proportionality constants for routine that use two PIDs

--- tensiometre/mechtest.py
+++ tensiometre/mechtest.py
@@ -8,6 +8,10 @@ from tensiometre import moverPID
 
 def move_to_constant_positions(ab2xy, outnames, dxs, dys, durations, kp=0.9, moveback=False):
     """Moving the absolute position of the head by dy (depth) and dx (width) and stay at that position using PID feedback. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
+    if not hasattr(kp, "__len__"):
+        kps = [kp, kp]
+    else:
+        kps = kp
     with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
         sensors = [sensorA, sensorB]
         #setting up sensors
@@ -20,8 +24,8 @@ def move_to_constant_positions(ab2xy, outnames, dxs, dys, durations, kp=0.9, mov
         #setting up PID
         pids = []
         initialposition = [x0, y0] + actuator.um2step(np.array([X0, Y0]))
-        for s in initialposition:
-            pid = PID(kp, 0, 0)
+        for s, k in zip(initialposition, kps):
+            pid = PID(k, 0, 0)
             pid.setPoint = s
             pids.append(pid)
         try:
@@ -45,11 +49,15 @@ def move_to_constant_positions(ab2xy, outnames, dxs, dys, durations, kp=0.9, mov
             if moveback:
                 #move the actuator back to its original position
                 actuator.move_to(x0, y0, z0)
-                
+
 def add_constant_deflection(ab2xy, outnames, dXs, dYs, durations, kp=0.9, moveback=False):
     """Considering initial deflection is 0 in x and successively add dX to the deflection during duration and stay at this deflection using PID feedback (constant stress). Iterate on next dX, dY, duration. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
 
     #remember original positions of the sensors and actuator
+    if not hasattr(kp, "__len__"):
+        kps = [kp, kp]
+    else:
+        kps = kp
     with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
         sensors = [sensorA, sensorB]
         #setting up sensors
@@ -62,20 +70,20 @@ def add_constant_deflection(ab2xy, outnames, dXs, dYs, durations, kp=0.9, moveba
         #setting up PID
         pids = []
         initialposition = (np.array([X0, Y0]))
-        for s in initialposition:
-            pid = PID(kp, 0, 0)
+        for s, k in zip(initialposition, kps):
+            pid = PID(k, 0, 0)
             pid.setPoint = s
             pids.append(pid)
         try:
             for dX, dY, duration, outname in zip(dXs, dYs, durations, outnames):
                 for s, pid in zip(
-                    np.array([X0,Y0]) + np.array([dX, dY]), 
+                    np.array([X0,Y0]) + np.array([dX, dY]),
                     pids
                 ):
                     pid.setPoint = s
                 with open(outname, "wb") as fout:
                     m = moverPID.constant_deflection_XY(
-                        sensors, actuator, 
+                        sensors, actuator,
                         ab2xy, pids, outputFile=fout
                         )
                     t0 = time.time()
@@ -93,10 +101,13 @@ def add_constant_deflection(ab2xy, outnames, dXs, dYs, durations, kp=0.9, moveba
             if moveback:
                 #move the actuator back to its original position
                 actuator.move_to(x0, y0, z0)
-                
+
 def add_constant_deflectionX_move_to_constant_positiony(ab2xy, outnames, dXs, dys, durations, kp=0.9, moveback=False, maxYdispl=None):
     """Considering initial deflection is (0,0), successively add dX to the deflection and  dy to absolute position and stay at this constnt X deflection and constant y position during duration. Stay at this deflection and positon using using PID feedback (constant stress and constant strain respectively). Iterate on next dX, duration. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
-          
+    if not hasattr(kp, "__len__"):
+        kps = [kp, kp]
+    else:
+        kps = kp
     #remember original positions of the sensors and actuator
     with closing(DT3100('169.254.3.100')) as sensorA, closing(DT3100('169.254.4.100')) as sensorB, closing(MPC385()) as actuator:
         sensors = [sensorA, sensorB]
@@ -112,14 +123,13 @@ def add_constant_deflectionX_move_to_constant_positiony(ab2xy, outnames, dXs, dy
         initialposition1 = np.array(X0)
         initialposition2 = y0 + actuator.um2step(np.array(Y0))
         initialposition  = [initialposition1,initialposition2]
-        for s in initialposition:
-            pid = PID(kp, 0, 0)
+        for s,k in zip(initialposition,kps):
+            pid = PID(k, 0, 0)
             pid.setPoint = s
             pids.append(pid)
-        
         try:
             for outname, dX, dy, duration in zip(outnames, dXs, dys, durations):
-                
+
                 ip = [np.array([X0]) + np.array([dX]), initialposition[1] + actuator.um2step(np.array([dy]))]
                 for s, pid in zip(ip, pids):
                     pid.setPoint = s
@@ -143,19 +153,19 @@ def add_constant_deflectionX_move_to_constant_positiony(ab2xy, outnames, dXs, dy
             if moveback:
                 #move the actuator back to its original position
                 actuator.move_to(x0, y0, z0)
-               
-def add_constant_deflectionX_stay_constant_positiony(outname, ab2xy,kp=0.9,dX=30, dy=0, duration=None, moveback=False):
+
+def add_constant_deflectionX_stay_constant_positiony(outname, ab2xy,kp=0.9,dX=30, dy=0, duration=None, moveback=False, maxYdispl=None):
     """Add a constant deflection of dx while staying at the same absolute position in y"""
-    add_constant_deflectionX_move_to_constant_positiony(ab2xy,outnames = [outname], dXs = [dX], dys=[dy], durations=[duration], kp=kp, moveback=moveback)
-    
+    add_constant_deflectionX_move_to_constant_positiony(ab2xy,outnames = [outname], dXs = [dX], dys=[dy], durations=[duration], kp=kp, moveback=moveback, maxYdispl= maxYdispl)
+
 def stay_constant_deflection(outname, ab2xy, kp=0.9, duration=None, moveback=False):
-    """keep the head at constant position from the sensor"""             
+    """keep the head at constant position from the sensor"""
     move_to_constant_deflection(ab2xy, outnames = [outname], dxs = [0], dys = [0],durations=[duration], kp=kp, moveback=moveback)
-    
+
 def move_to_constant_position(outname, ab2xy, kp=0.9, dy =-100, dx=0, duration=None, moveback=False):
     """Moving the absolute position of the head by dy (depth) and dx (width) and stay at that position using PID feedback. Need ab2xy calibration matrix. Duration is in seconds. If None specified, continues until stopped."""
     move_to_constant_positions(ab2xy, outnames=[outname], dxs=[dx], dys=[dy], durations=[duration], kp=kp, moveback=moveback)
-                
+
 def stay_constant_position(outname, ab2xy, kp=0.9, duration=None, moveback=False):
     """Keep the absolute position of the head constant using PID feedback. Need ab2xy calibration matrix."""
     move_to_constant_position(outname, ab2xy, kp, dy=0, dx=0, duration=duration, moveback=False)
@@ -219,4 +229,3 @@ def shear(dx = 3000, velocity=2, duration = 1., avt=3, avn=3, outputname=None):
             np.save(outputname+'.npy', np.column_stack((t,valuesA, valuesB)))
         finally:
             actuator.move_to(x0, y0, z0)
-            
