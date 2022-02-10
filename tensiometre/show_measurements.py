820589fbbd9d7d73905693dc68a59cef3edc007b
mleocmach <mathieu.leocmach@univ-lyon1.fr>
2022-02-10 17:32:42 +0100

    Can give a list of ip adresses to plot several sensors at once. Finishes on window closing

--- tensiometre/show_measurements.py
+++ tensiometre/show_measurements.py
@@ -1,10 +1,10 @@
 import time
 import numpy as np
-from contextlib import closing
+from contextlib import closing, ExitStack
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 from IPython import display
-from tensiometre.dt3100 import DT3100, ureg
+from tensiometre.dt3100 import DT3100, ureg, recover
 from threading import Thread
 
 #physical units
@@ -39,23 +39,29 @@ class Updater(Thread):
 
 class Shower:
     """A class to show a figure for a sensor"""
-    def __init__(self, ringbuff, dt, ymin, ymax, name='DT3100', sleeptime=0.1):
-        self.ringbuff = ringbuff
-        ndisplayed = len(self.ringbuff)
+    def __init__(self, ringbuffs, dts, ymin, ymax, name='DT3100', sleeptime=0.1, tw=1, names=None):
+        if names is None:
+            names = [f'sensor {i:d}' for i in range(len(ringbuffs))]
+        self.ringbuffs = ringbuffs
+        self.dts = dts
+        self.st = sleeptime
         #create empty plot
         self.fig, self.ax = fig, ax = plt.subplots(num=name)
         self.fig.clf()
-        #plt.show()
-        self.hl, = plt.plot(dt * np.arange(ndisplayed), self.ringbuff)
         #set x and y range
-        plt.xlim(0, dt.m * ndisplayed)
-        plt.xlabel('time (%s)' % dt.u)
+        plt.xlim(0, tw)
+        plt.xlabel('time (s)')
         plt.ylim(ymin, ymax)
         plt.ylabel('distance (µm)')
-        self.st = sleeptime
+        self.hl = []
+        for ringbuff, dt, label in zip(self.ringbuffs, self.dts, names):
+            self.hl.append(plt.plot(dt * np.arange(len(ringbuff)), ringbuff, label=label)[0])
+        plt.legend()
+
         def animate(i):
-            self.hl.set_ydata(self.ringbuff)  # update the data.
-            return self.hl,
+            for line, ringbuff in zip(self.hl, self.ringbuffs):
+                line.set_ydata(ringbuff)  # update the data.
+            return self.hl
         self.ani = animation.FuncAnimation(
                 self.fig, animate, interval=self.st*1000, blit=True, save_count=1, repeat=True)
         #plt.show()
@@ -78,46 +84,58 @@ class Shower:
     #         plt.draw()
     #         plt.pause(self.st)
 
-def show_measurement(sensors, ndisplayed = 2**13, chunk = None, ymin=None, ymax=None):
+def show_measurement(sensors, tw=1, ymin=None, ymax=None, names=None):
     """Display measurements in a rolling graph"""
-    if chunk is None:
+    ringbuffs = []
+    updts = []
+    for sensor in sensors:
+        ndisplayed = int(Q_(tw, 's')/sensor.unit_time())
+        ringbuff = np.zeros(ndisplayed, float)
+        ringbuffs.append(ringbuff)
         chunk = 2**int(np.log2(ndisplayed/32))
-    #unit time per sample
-    dt = capteur.unit_time()
-    ringbuff = np.zeros(ndisplayed, float)
-    ringbuff[:] = capteur.readN(ndisplayed)
+        updts.append(Updater(ringbuff, sensor, chunk))
+
+    for updt in updts:
+        updt.start()
+
+
+
+
+    dts = [sensor.unit_time() for sensor in sensors]
     #set x and y range
     if ymin is None:
-        ymin = capteur.sensor.smr.m
+        ymin = min(sensor.sensor.smr.m for sensor in sensors)
     if ymax is None:
-        ymax = capteur.sensor.emr.m
-
-
+        ymax = max(sensor.sensor.emr.m for sensor in sensors)
 
     shw = Shower(
-        ringbuff, dt, ymin, ymax,
+        ringbuffs, dts, ymin, ymax, tw=tw, names=names,
         #name='DT3100-%d'%capteur.sensor.sn,
         #sleeptime=0.1
     )
-    updt = Updater(ringbuff, capteur, chunk)
-    updt.start()
     plt.show()
+    fig = plt.gcf()
+    #fig.axes[0].has_been_closed = False
+    #fig.canvas.mpl_connect('close_event', on_close)
     try:
-        while updt.is_alive():
-            #shw()
-            pass
+        while min(updt.is_alive() for upt in updts):
+            active_fig_managers = plt._pylab_helpers.Gcf.figs.values()
+            if fig not in active_fig_managers:
+                break
 
     except KeyboardInterrupt:
         pass
     finally:
-        updt.go = False
-        updt.join()
+        for updt in updts:
+            updt.go = False
+        for updt in updts:
+            updt.join()
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Display measurements from a DT3100 sensor in a rolling graph.')
-    parser.add_argument('ip', nargs='?', default='169.254.3.100', help='IP address of the DT3100.')
+    parser.add_argument('ip', nargs='*', help='IP addresses of the DT3100 (default 169.254.3.100).')
     parser.add_argument(
         '--avt', default=0, type=int,
         help=DT3100.set_averaging_type.__doc__
@@ -130,7 +148,13 @@ if __name__ == '__main__':
     parser.add_argument('--ymax', default=None, type=float, help='maximum distance')
     parser.add_argument('--tw', default=1.0, type=float, help='Width of the time window, in seconds.')
     args = parser.parse_args()
-    with closing(DT3100(args.ip)) as capteur:
-        capteur.set_averaging_type(args.avt)
-        capteur.set_averaging_number(args.avn)
-        show_measurement(capteur, ndisplayed = int(Q_(args.tw, 's')/capteur.unit_time()), ymin=args.ymin, ymax=args.ymax)
+    if args.ip is None or len(args.ip)==0:
+        args.ip = ['169.254.3.100']
+    for ip in args.ip:
+        recover(ip)
+    with ExitStack() as stack:
+        sensors = [stack.enter_context(closing(DT3100(ip))) for ip in args.ip]
+        for sensor in sensors:
+            sensor.set_averaging_type(args.avt)
+            sensor.set_averaging_number(args.avn)
+        show_measurement(sensors, args.tw, ymin=args.ymin, ymax=args.ymax, names=args.ip)
